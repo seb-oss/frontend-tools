c406028437ea012baa98f61a8815e2d3cf9008c8
Yousif Al-Raheem <yousifalraheem@gmail.com>
2019-12-06 07:46:38 +0800

    refactor(cookiestorage): refactored cookie storage, fixed bugs, and test

--- src/cookieStorage.ts
+++ src/cookieStorage.ts
@@ -1,9 +1,17 @@
+export interface SetItemOptions {
+    /** Expiration Date */
+    expires?: Date;
+    /** Maximum age in seconds */
+    maxAge?: number;
+    /** Secure cookie. Set the security to `true` if you want it to be encrypted in **HTTPS** connections */
+    secure?: boolean;
+}
 /**
  * CookieStorage is a handler for reading and writing cookies in Javascript
  * Usage is similar to `localStorage` and `sessionStorage`
  */
 export class CookieStorage implements Storage {
-    get length(): number { return this.keys.length; }
+    get length(): number { return this.keys().length; }
 
     /**
      * Retrieve an item from the stored cookie
@@ -21,59 +29,44 @@ export class CookieStorage implements Storage {
      * Sets an item to the stored cookie
      * @param key The key of the cookie to be set
      * @param value The value of the cookie
-     * @param options Available options:
-     * - `expires`: Expiration period. You can pass a `number` representing the number of days, or a `Date` object
-     * - `path`: The path of the specified cookie. (e.g. `/dashboard`)
-     * - `domain`: The subdomain of the specified cookie. (e.g. `foo.example.com`)
-     * - `secure`: Set the security to `true` if you want it to be encrypted in **HTTPS** connections
+     * @param options Available options are `expires`, `maxAge` and `secure` @see SetItemOptions interface for reference
      * @returns `true` if successful
      */
-    setItem(key: string, value: string, options: { expires?: number | Date, path?: string, domain?: string, secure?: string } = {}): boolean {
+    setItem(key: string, value: any, options: SetItemOptions = {}): boolean {
         if (!key || /^(?:expires|max\-age|path|domain|secure)$/i.test(key)) {
             return false;
         }
-        let sExpires = "";
-        if (options && options.expires) {
-            switch (options.expires.constructor) {
-                case
-                    Number: sExpires = options.expires === Infinity
-                        ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
-                        : "; expires=" + (new Date((options.expires as number) * 1e3 + Date.now())).toUTCString();
-                            break;
-                case Date: sExpires = "; expires=" + (options.expires as Date).toUTCString(); break;
+        let expires: string = "";
+        let maxAge: string = "";
+        const secure: string = options && options.secure ? "; Secure" : "";
+        if (options) {
+            if (options.expires && options.expires instanceof Date) {
+                expires = "; Expires=" + options.expires.toUTCString();
+            }
+            if (options.maxAge && typeof options.maxAge === "number" && options.maxAge !== Infinity) {
+                maxAge = `; Max-age=${options.maxAge}`;
             }
         }
-        document.cookie = encodeURIComponent(key)
-            + "="
-            + encodeURIComponent(value)
-            + sExpires
-            + (options && options.domain ? "; domain=" + options.domain : "")
-            + (options && options.path ? "; path=" + options.path : "")
-            + (options && options.secure ? "; secure" : "");
-        return true;
+        document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}${expires}${maxAge}${secure}`;
+        return this.hasItem(key);
     }
 
     /**
      * Remove an item from the stored cookie
-     * @param key The key of the cookie to be removed
-     * @param options Available options:
-     * - `path`: The path of the specified cookie. (e.g. `/dashboard`)
-     * - `domain`: The subdomain of the specified cookie. (e.g. `foo.example.com`)
-     * @returns `true` if successful
+     * @param {string} key The key of the cookie to be removed
+     * @returns {boolean} `true` if successful
      */
-    removeItem(key: string, options?: { path?: string, domain?: string }): boolean {
-        if (!this.hasItem(key)) { return false; }
-        document.cookie = encodeURIComponent(key)
-            + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT"
-            + (options && options.domain ? "; domain=" + options.domain : "")
-            + (options && options.path ? "; path=" + options.path : "");
-        return true;
+    removeItem(key: string): boolean {
+        if (this.hasItem(key)) {
+            document.cookie = `${encodeURIComponent(key)}=; max-age=-1`;
+        }
+        return !this.hasItem(key);
     }
 
     /**
      * Verifies if an item exists in the cookie
-     * @param key The key of the item to be verified
-     * @returns `true` if it exists
+     * @param {string} key The key of the item to be verified
+     * @returns {boolean} `true` if it exists
      */
     hasItem(key: string): boolean {
         if (!key || /^(?:expires|max\-age|path|domain|secure)$/i.test(key)) {
@@ -83,13 +76,14 @@ export class CookieStorage implements Storage {
     }
 
     clear(): void {
-        const cookies = document.cookie.split(";");
+        const cookies: Array<string> = document.cookie.split(";");
 
-        for (let i = 0; i < cookies.length; i++) {
+        for (let i: number = 0; i < cookies.length; i++) {
             const cookie: string = cookies[i];
             const eqPos: number = cookie.indexOf("=");
             const name: string = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            const value: string = eqPos > -1 ? cookie.substr(eqPos + 1, cookie.length) : "";
+            document.cookie = `${name}${value ? "=" : ""}; max-age=-1`;
         }
     }
 
@@ -98,14 +92,18 @@ export class CookieStorage implements Storage {
      * @returns The list of keys in the stored cookie
      */
     keys(): Array<string> {
-        const aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
-        for (let nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) {
+        const aKeys: Array<string> = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+        for (let nLen: number = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) {
             aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]);
         }
         return aKeys;
     }
 
+    /**
+     * @param index The index of the array of keys
+     * @returns The key at the given index, if any
+     */
     key(index: number): string {
-        return this.keys[index];
+        return this.keys()[index];
     }
 }
