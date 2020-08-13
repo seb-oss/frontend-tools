09b51b4ebe564b516f546263890435ef88a51685
Yousif Al-Raheem <yousifalraheem@gmail.com>
2020-08-13 17:32:00 +0800

    fix(isphonenumber): fix isphonenumber validation to match 4-15 digits
    Following the standard validation rules online, it seems that phone numbers can have a length of
    4-15 digits long. This fix address that.

--- src/isPhoneNumber/isPhoneNumber.test.ts
+++ src/isPhoneNumber/isPhoneNumber.test.ts
@@ -12,7 +12,10 @@ describe("Util: isPhoneNumber", () => {
         { statement: "Should return false for null", params: null, result: false },
         { statement: "Should return false for random string", params: "asbcdasdf", result: false },
         { statement: "Should return true for valid phone", params: "06449561629", result: true },
-        { statement: "Should return true for valid phone", params: "6449561629", result: true },
+        { statement: "Should accept numbers that are 4 digits long", params: "1234", result: true },
+        { statement: "Should not accept numbers less than 4 digits long", params: "123", result: false },
+        { statement: "Should accept numbers that are 15 digits long", params: "012345678912345", result: true },
+        { statement: "Should not accept numbers longer than 15 digits long", params: "0123456789123456", result: false },
     ];
     testCases.map((testCase: TestCase) => {
         it(`${testCase.statement} | params: ${JSON.stringify(testCase.params)} | result: ${testCase.result}`, () => {
