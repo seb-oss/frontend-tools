4edae0795f99406ef22da00d54637697606433ae
Subotic <S7386B@corp1.ad1.seb.net>
2020-08-19 11:13:15 +0800

    feat: adds missing date utils

---
+++ src/isDateBefore/isDateBefore.ts
@@ -0,0 +1,15 @@
+import { isValidDate } from "../isValidDate";
+
+/**
+ * Compare two dates and return true if the first is less (or before) than the second ignoring the time
+ * @param {Date} a The first date
+ * @param {Date} b The second date
+ * @returns {boolean} True if date `a` comes before date `b`
+ */
+export function isDateBefore(a: Date, b: Date): boolean {
+    if (!a || !b || !isValidDate(a) || !isValidDate(b)) {
+        return false;
+    } else {
+        return a.getFullYear() < b.getFullYear() || a.getMonth() < b.getMonth() || a.getDate() < b.getDate();
+    }
+}
