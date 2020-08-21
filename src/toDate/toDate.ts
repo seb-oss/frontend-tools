fddcd69f8aab748cdf560e1c7cb22cf9973b8225
Subotic <S7386B@corp1.ad1.seb.net>
2020-08-21 15:20:58 +0800

    style: fixes linting errors

--- src/toDate/toDate.ts
+++ src/toDate/toDate.ts
@@ -11,19 +11,23 @@ export function toDate(
     value: Date | string | number | DateComponents | null | undefined,
     /** DEPRACATED! The input format has been depracated. The default javascript Date object string constructor will be used instead  */
     inputFormat?: any
-)   {
+): Date | null   {
     if (inputFormat) {
         console.warn("The inputFormat has been depracated. The default javascript Date object string constructor will be used instead");
     }
     switch(true) {
         case !value: return null;
-        case value instanceof Date: return value;
-        case typeof value === "string" || typeof value === "number":
-            const newDate: Date = new Date(value);
+        case value instanceof Date: {
+            return isValidDate(value as Date) ? value as Date : null;
+        };
+        case typeof value === "string" || typeof value === "number": {
+            const newDate: Date = new Date(value as string | number);
             return isValidDate(newDate) ? newDate : null;
-        case typeof value === "object" && Array.isArray(value) && value.length > 1:
+        }
+        case typeof value === "object" && Array.isArray(value) && value.length > 1: {
             const newDate: Date = new Date(...value as DateComponents);
             return isValidDate(newDate) ? newDate : null;
+        }
+        default: return null;
     }
-    return null;
 }
