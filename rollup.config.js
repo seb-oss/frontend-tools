964824cd474b7507afaff7e68fb64a492c716c46
Mario Subotic <mario.subotic@gmail.com>
2022-01-12 13:53:35 +0800

    build: fix package json path in postbuild script

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
-import commonPkg from "../package.json";
+import commonPkg from "./package.json";
 
 const components = require("./src/index.json");
 
