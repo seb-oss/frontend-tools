a048537b0f8b0552c631d6b76e70653400b0f805
s6111f <kayle.san-juan@seb.se>
2022-06-15 14:10:26 +0800

    fix: 🐛 correct logic to determine if date is after second date
    ✅ Closes: #209

--- src/isDateAfter/isDateAfter.test.ts
+++ src/isDateAfter/isDateAfter.test.ts
@@ -10,9 +10,11 @@ describe("Util: isDateAfter", () => {
     const testCases: Array<TestCase> = [
         { statement: "Should return false if firstDate is invalid", date1: "abcd", date2: new Date(), result: false},
         { statement: "Should return false if secondDate is invalid", date1: new Date(), date2: "new Date()", result: false},
-        { statement: "Should return true if date is after date1", date1: new Date("2019-12-11"), date2: new Date("2019-11-11"), result: true},
-        { statement: "Should return false if date is not after date1", date1: new Date("2019-11-11"), date2: new Date("2019-11-12"), result: false},
-        { statement: "Should return false if date is not after date1", date1: new Date("2019-11-11"), date2: new Date("2019-11-11"), result: false}
+        { statement: "Should return true if date1 is after date2", date1: new Date("2019-12-11"), date2: new Date("2019-11-11"), result: true},
+        { statement: "Should return true if date1 is after date2", date1: new Date("2019-05-06"), date2: new Date("1952-05-06"), result: true},
+        { statement: "Should return false if date1 is not after date2", date1: new Date("2019-11-11"), date2: new Date("2019-11-12"), result: false},
+        { statement: "Should return false if date1 is not after date2", date1: new Date("2011-07-01"), date2: new Date("2018-04-01"), result: false},
+        { statement: "Should ignore the time and return false if the two dates are equal", date1: new Date("2019-11-11 23:00:00"), date2: new Date("2019-11-11 11:00:00"), result: false},
     ];
     testCases.map((testCase: TestCase) => {
         test(`- ${testCase.statement} | params: ${JSON.stringify({date1: testCase.date1, date2: testCase.date2})} | result: ${testCase.result}`, () => {
